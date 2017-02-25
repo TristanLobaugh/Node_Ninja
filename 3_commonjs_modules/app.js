5458aa72212b90dcaf1e437f485fad400748ac6e
TristanLobaugh <ttrader_atl@yahoo.com>
2017-02-25 13:13:43 -0500

    commit 3 adding modules

--- 3_commonjs_modules/app.js
+++ 3_commonjs_modules/app.js
@@ -1,14 +1,9 @@
+const math = require('./math');
 
 const command = process.argv[2];
-const c = Number(process.argv[3]);
-const d = Number(process.argv[4]);
+const a = Number(process.argv[3]);
+const b = Number(process.argv[4]);
 
-if (command === 'add') {
-	console.log(`Add ${c} + ${d} = ${c + d}`);
-} else if (command === 'subtract') {
-	console.log(`Add ${c} - ${d} = ${c - d}`);
-} else if (command === 'multiply') {
-	console.log(`Add ${c} * ${d} = ${c * d}`);
-} else if (command === 'divide') {
-	console.log(`Add ${c} / ${d} = ${c / d}`);
-}
+const value = math[command](a, b);
+
+console.log(value);
