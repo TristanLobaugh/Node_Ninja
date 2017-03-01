76df1581944029df5c6720fbde802489d22f7758
TristanLobaugh <ttrader_atl@yahoo.com>
2017-03-01 12:10:56 -0500

    commit 19 securing passwords with bcrypt
    hash passwords with bcrypt-nodejs

--- 17-authentication/passport.js
+++ 17-authentication/passport.js
@@ -1,21 +1,22 @@
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
+import bcrypt from 'bcrypt-nodejs';
 import { db } from './db';
 // const LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy(authenticate));
 
 function authenticate(email, password, done) {
 	db('users')
 	.where('email', email)
 	.first()
 	.then(user => {
-		if (!user || user.password !== password) {
+		if (!user || !bcrypt.compareSync(password, user.password)) {
 			return done(null, false, { message: 'invalid user and password combination' });
 		}
 		done(null, user);
 	}, done);
 }
+
+passport.use(new LocalStrategy(authenticate));
 
 passport.serializeUser((user, done) => {
 	done(null, user.id);
